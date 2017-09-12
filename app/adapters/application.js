a31074dc29de7eeb0a142824c5ed2dee1487c67f
ivanlobodyuk <ilobodyuk@mindk.com>
2017-09-12 13:13:25 +0300

    feat(model) get query for all boards work

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://boards-api.sys/app_dev.php/'
+    host: 'http://boards-api.sys/app_dev.php',
+    namespace: 'api',
 });
--- app/controllers/boards.js
+++ app/controllers/boards.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-     comments: 'SHHHHITTTTTTTT'
+     queryParams: ['search'],
+
+     filteredBoards: Ember.computed('search', function() {
+        var search = this.get('category');
+        var boards = this.get('model');
+        console.log('boards ', boards);
+        if (search) {
+          return boards.filterBy('name', search);
+        } else {
+          return boards;
+        }
+      })
 });
