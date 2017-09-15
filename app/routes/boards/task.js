0718ef03f213fd63b112d29a8d6c95440174c98e
ivanlobodyuk <ilobodyuk@mindk.com>
2017-09-15 11:06:42 +0300

    feat(model) save task for model is work

--- app/routes/boards/task.js
+++ app/routes/boards/task.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
 
   model(params) {
     return Ember.RSVP.hash({
-      task: this.get('store').createRecord('task',{'board':1}),
+      task: this.get('store').createRecord('task',{'board':params.board_id}),
       board: this.get('store').findAll('board'),
       boardItem: this.get('store').findRecord('board',params.board_id)
     });
