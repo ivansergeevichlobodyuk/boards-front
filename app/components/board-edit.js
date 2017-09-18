1183b8e25ea4030f6046969348af55045ab6545c
ivanlobodyuk <ilobodyuk@mindk.com>
2017-09-18 19:41:30 +0300

    feat(model) save task for model is work

--- app/components/board-edit.js
+++ app/components/board-edit.js
@@ -3,20 +3,29 @@ export default Ember.Component.extend({
 
   store: Ember.inject.service(),
   showEdit: false,
+  validationData: new Array(),
+  susccessEdited(){
+    this.toggleProperty('showEdit');
+    UIkit.notify("Board name was changed");
 
+  },
   actions:{
     editBoard(){
       this.toggleProperty('showEdit');
     },
-
     saveBoard(item){
-      item.save().then(function(){
-        this.toggleProperty('showEdit');
-        UIkit.notify("Board name was changed");
-      })
-      this.toggleProperty('showEdit');
+      var _this = this;
+      item.save().then( ()=> {
+        this.susccessEdited();
+        this.set('validationData', new Array());
+      },() =>{
+        var validation = new Array();
+        console.log("errors ",item.get('errors.name'));
+         item.get('errors.name').forEach(function(val,inx){
+           validation[val.attribute] = val.message;
+         });
+         this.set('validationData', validation);
+      });
     }
-
   }
-
 });
