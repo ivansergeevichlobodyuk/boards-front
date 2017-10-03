42f2effabbf47a7348c3f121fda5bfe9258fdf30
ivanlobodyuk <ilobodyuk@mindk.com>
2017-10-03 18:49:50 +0300

    feat(model) save task for model is work

--- app/controllers/boards/taskitem.js
+++ app/controllers/boards/taskitem.js
@@ -8,23 +8,27 @@ export default Ember.Controller.extend({
   },
   actions: {
     submition(){
-      this.get('model.task').save().then(() => {
+      //this.set('model.task.board', $("#tag-type-select").find(":selected").val());
+      let task = this.get('model.task');
+      task.set('board', this.get('store').peekRecord('board', $("#tag-type-select").find(":selected").val()));
+      task.save().then(() => {
           this.get('store').findAll('board');
           UIkit.notify("New board was added");
           this.transitionToRoute('boards');
           this.set('validationData', new Array());
         },
         (error) => {
-          var validation = new Array();
-          error.errors.forEach(function (val, inx) {
-            validation[val.source.pointer] = val.detail;
-          });
-          this.set('validationData', validation);
+           console.log('errrorrr ', error);
+          // var validation = new Array();
+          // error.errors.forEach(function (val, inx) {
+          //   validation[val.source.pointer] = val.detail;
+          // });
+          // this.set('validationData', validation);
         }
       )
     },
 
-    selectBoard(){
+    selectBoard( ){
       this.set('model.task.board', $("#tag-type-select").find(":selected").val());
     }
 
